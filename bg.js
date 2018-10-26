6373206f83f0bf9cdffd1c35b849ec7472d3ba07
gmourafilho <gmoura_filho@hotmail.com>
2018-10-26 09:03:43 -0300

    Outros endereços e Previsão saída
    Mudançs:
    - Outros endereços mapeados;
    - Previsão de Saída quando menos de três batidas;
    - Omite campos quando ainda não bateu o ponto.

--- bg.js
+++ bg.js
@@ -1,15 +1,13 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (~tab.url.indexOf('/pontoweb')) {
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (~tab.url.indexOf('pontoweb')) {
     chrome.pageAction.show(tabId);
     chrome.tabs.query({
       active: true,
       currentWindow: true
-    }, function (tabs) {
+    }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, "calcularSaida");
     });
   }
-});
-
-chrome.pageAction.onClicked.addListener(function (tab) {
-  chrome.tabs.create({ url: "http://pontoeletronico.capgv.intra.bnb/pontoweb", "active": true });
 });
--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    $(document).ready(function () {
+  function(request, sender, sendResponse) {
+    $(document).ready(function() {
       calcularSaida()
     });
     sendResponse();
@@ -8,9 +8,11 @@ chrome.runtime.onMessage.addListener(
 );
 
 function calcularSaida() {
+  var appendLabel = false;
   var cargaHoraria1 = "5:45";
   var cargaHoraria2 = "8:00";
   var batidas = $("#batidas").find("td");
+  console.log(batidas.length)
 
   if (batidas.length >= 3) {
     var batida1 = batidas[0].textContent.split(" ")[1];
@@ -18,10 +20,23 @@ function calcularSaida() {
     var batida3 = batidas[2].textContent.split(" ")[1];
     var batida4_6h = convertHourToMinute(batida1) + convertHourToMinute(cargaHoraria1) + convertHourToMinute(batida3) - convertHourToMinute(batida2);
     var batida4_8h = convertHourToMinute(batida1) + convertHourToMinute(cargaHoraria2) + convertHourToMinute(batida3) - convertHourToMinute(batida2);
+    appendLabel = true;
+  }else if(batidas.length >= 1)
+  {
+    //Apenas uma batida
+    var batida1 = batidas[0].textContent.split(" ")[1]
+    //Previsão de 15 minutos de intervalo
+    var batida4_6h = convertHourToMinute(batida1) + convertHourToMinute(cargaHoraria1) + convertHourToMinute("00:15");
+    //Previsão de 30 minutos de intervalo
+    var batida4_8h = convertHourToMinute(batida1) + convertHourToMinute(cargaHoraria2) + convertHourToMinute("00:30");
+    appendLabel = true;
   }
 
-  $(".label").parent().append('<span class="label label-default">Saída estimada 6h: <strong>' + convertMinutesToHour(batida4_6h) + '<strong></span> ');
-  $(".label").parent().append('<span class="label label-default">Saída estimada 8h: <strong>' + convertMinutesToHour(batida4_8h) + '<strong></span> ');
+  if(appendLabel)
+  {
+    $(".label").parent().append('<span class="label label-default">Saída estimada 6h: <strong>' + convertMinutesToHour(batida4_6h) + '<strong></span> ');
+    $(".label").parent().append('<span class="label label-default">Saída estimada 8h: <strong>' + convertMinutesToHour(batida4_8h) + '<strong></span> ');
+  }
 }
 
 function convertHourToMinute(time) {
